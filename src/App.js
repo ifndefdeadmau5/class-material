0dc6fbd7a5af97e80ecb1693bdb22ebf96bcd91d
Trevor <trevorme91@gmail.com>
2019-06-18 20:58:12 +0900

    imperative version

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import List from './List';
 import './App.css';
 
 function App() {
+  const [input, setInput] = useState('');
+  // const [todos, setTodos] = useState([]);
+
+  let todos = ['banana'];
+
+  console.log('todos:', todos);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type="text"
+        value={input}
+        onChange={event => setInput(event.target.value)}
+      />
+      <button
+        onClick={() => {
+          todos = [...todos, input];
+          const listElement = document.getElementById('list');
+          const newTodo = document.createTextNode(input); 
+          listElement.appendChild(newTodo)
+        }}
+      >
+        확인
+      </button>
+      <List id="list">
+        {todos}
+      </List>
     </div>
   );
 }
 
 export default App;
+
+
